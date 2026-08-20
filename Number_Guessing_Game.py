109334afc556343e482f306df2fc9b74a7a686f2
Nischal Bhandari <bhandarinischal45@gmail.com>
2026-08-20 21:06:34 +0545

    Implement loop for guessing num.

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,2 +1,15 @@
 import random
 num = random.randint(1,100)
+a = int(input("Guess a number between 1 to 100 : \n"))
+c = 1
+while(num!=a):
+    if(num > a):
+        print("Higher Number Please !\n")
+    else:
+        print("Lower Number please \n")
+    a = int(input("Guess a number between 1 to 100 : \n"))
+    c = c+1
+    
+print("Number guessed Correctly !! \n You Won !!")
+print(f"You have guessed the number correctly in {c} attempts")
+    
