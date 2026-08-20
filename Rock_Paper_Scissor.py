8f0d0cb69ceff81ce2a7f89d490b31bf24bafe6c
Nischal Bhandari <bhandarinischal45@gmail.com>
2026-08-20 18:45:09 +0545

    add logic for game

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -6,3 +6,25 @@ a = input("Choose rock, paper or scissors: \n").lower()
 while a not in choices:
     print("Invalid choice! Please choose rock, paper or scissors.\n")
     a = input("Choose rock, paper or scissors: \n").lower()
+print("Computer chooses:", computer)
+
+if a == computer:
+    print("It's a Draw!")
+
+elif a == "rock":
+    if computer == "scissors":
+        print("You Won!")
+    else:
+        print("Computer Won!")
+
+elif a == "paper":
+    if computer == "rock":
+        print("You Won!")
+    else:
+        print("Computer Won!")
+
+elif a == "scissors":
+    if computer == "paper":
+        print("You Won!")
+    else:
+        print("Computer Won!")
